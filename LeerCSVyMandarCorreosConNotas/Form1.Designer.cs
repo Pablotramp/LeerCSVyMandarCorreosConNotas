e433dac992c702a62b7bf9b30f68ce4d26ae2a08
sebastian cifuentes marin <s7c7m7@hotmail.com>
2024-04-05 14:43:07 +0200

    Agregar archivos de proyecto.

---
+++ LeerCSVyMandarCorreosConNotas/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace LeerCSVyMandarCorreosConNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCargarFichero = new System.Windows.Forms.Button();
+            this.LblRutaFichero = new System.Windows.Forms.Label();
+            this.BtnEnviarEmails = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // BtnCargarFichero
+            // 
+            this.BtnCargarFichero.Location = new System.Drawing.Point(113, 58);
+            this.BtnCargarFichero.Name = "BtnCargarFichero";
+            this.BtnCargarFichero.Size = new System.Drawing.Size(92, 23);
+            this.BtnCargarFichero.TabIndex = 0;
+            this.BtnCargarFichero.Text = "Cargar Fichero";
+            this.BtnCargarFichero.UseVisualStyleBackColor = true;
+            this.BtnCargarFichero.Click += new System.EventHandler(this.BtnCargarFichero_Click);
+            // 
+            // LblRutaFichero
+            // 
+            this.LblRutaFichero.AutoSize = true;
+            this.LblRutaFichero.Location = new System.Drawing.Point(239, 63);
+            this.LblRutaFichero.Name = "LblRutaFichero";
+            this.LblRutaFichero.Size = new System.Drawing.Size(42, 13);
+            this.LblRutaFichero.TabIndex = 1;
+            this.LblRutaFichero.Text = "Fichero";
+            // 
+            // BtnEnviarEmails
+            // 
+            this.BtnEnviarEmails.Location = new System.Drawing.Point(113, 106);
+            this.BtnEnviarEmails.Name = "BtnEnviarEmails";
+            this.BtnEnviarEmails.Size = new System.Drawing.Size(92, 23);
+            this.BtnEnviarEmails.TabIndex = 2;
+            this.BtnEnviarEmails.Text = "Enviar Emails";
+            this.BtnEnviarEmails.UseVisualStyleBackColor = true;
+            this.BtnEnviarEmails.Click += new System.EventHandler(this.BtnEnviarEmails_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnEnviarEmails);
+            this.Controls.Add(this.LblRutaFichero);
+            this.Controls.Add(this.BtnCargarFichero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCargarFichero;
+        private System.Windows.Forms.Label LblRutaFichero;
+        private System.Windows.Forms.Button BtnEnviarEmails;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LeerCSVyMandarCorreosConNotas/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LeerCSVyMandarCorreosConNotas
+{
+    public partial class Form1 : Form
+    {
+        public static System.IO.FileInfo infoFichero;
+        public static List<Alumno> listaAlumnos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LblRutaFichero.Text = "";
+        }
+
+        private static List<Alumno> CargarFichero(string rutaFichero)
+        {
+            listaAlumnos = new List<Alumno> { };
+            string[] entrada;
+            using (StreamReader ficheroLectura = new StreamReader(rutaFichero))
+            {
+                ficheroLectura.ReadLine(); // leo la cabecera para descartarla
+                int noRegistros = 0;
+                int sucesful = 0;
+                while (!ficheroLectura.EndOfStream)
+                {
+                    noRegistros++;
+                    try
+                    {
+                        entrada = ficheroLectura.ReadLine().Split(';');
+                        listaAlumnos.Add(new Alumno(int.Parse(entrada[0]), entrada[1], entrada[2], decimal.Parse(entrada[3])));
+                        sucesful++;
+                    }
+                    catch (Exception) { }
+                }
+                ficheroLectura.Close();
+                if (noRegistros > sucesful) MessageBox.Show($"{noRegistros - sucesful} alumno(s) no ha(n) sido añadido(s) correctamente");
+            }
+            return listaAlumnos;
+        }
+        private void BtnCargarFichero_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                infoFichero = new System.IO.FileInfo(openFileDialog1.FileName);
+                LblRutaFichero.Text = infoFichero.Name;
+            }
+        }
+        private void BtnEnviarEmails_Click(object sender, EventArgs e)
+        {
+            if (LblRutaFichero.Text != "")
+            {
+                CargarFichero(infoFichero.FullName);
+                string profesorEmail = "ptramen.cec@gmail.com";
+                string profesorPass = "vdvvixfklcyejvfm";
+
+                System.Net.Mail.SmtpClient smtpGmail = new System.Net.Mail.SmtpClient();
+                System.Net.Mail.MailMessage miCorreo = new System.Net.Mail.MailMessage();
+                System.Net.NetworkCredential misDatos = new System.Net.NetworkCredential(profesorEmail, profesorPass);
+
+                // configuracion del correo saliente
+                smtpGmail.Host = "smtp.gmail.com";  // servidor         (se mira en la documentación del servidor)
+                smtpGmail.Port = 587;               // puerto saliende  (se mira en la documentación del servidor)
+                smtpGmail.EnableSsl = true;         // certificado
+                smtpGmail.Credentials = misDatos;   // mail y password  (se genera uno para aplicaciones externas en la configuración de la cuenta)
+
+                int enviados = 0;
+
+                // configuración mensaje
+                foreach (Alumno alumno in listaAlumnos)
+                {
+                    try
+                    {
+                        miCorreo.To.Add(alumno.Email_Alumno);
+                        miCorreo.To.Add(profesorEmail);
+                        miCorreo.Subject = "Calificación curso AFD";
+                        miCorreo.Body = alumno.ToString();
+                        miCorreo.From = new System.Net.Mail.MailAddress(profesorEmail, "Calificación curso AFD");
+
+                        smtpGmail.Send(miCorreo);
+                        enviados++;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.ToString().Contains("cliente no se autenticó")) MessageBox.Show("La dirección de correo o la contraseña del remitente son erróneas", "Error de autentificación: ");
+                        else if (ex.ToString().Contains("no tiene la forma obligatoria para una direcc")) MessageBox.Show($"La dirección de correo de {alumno.Nombre_Alumno} es incorrecta", "Direccion de Correo Erronea: ");
+                        else MessageBox.Show(ex.ToString(), "Error genérico: ");
+                    }
+                }
+                MessageBox.Show($"Se han enviado {enviados} de {listaAlumnos.Count} correos", "CUIDADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+    public class Alumno
+    {
+        public int Id_Alumno;
+        public string Nombre_Alumno;
+        public string Email_Alumno;
+        public decimal Calificacion_Alumno;
+        public bool Aprobado;
+
+        public Alumno(int id_Alumno, string nombre_Alumno, string email_Alumno, decimal calificacion_Alumno)
+        {
+            Id_Alumno = id_Alumno;
+            Nombre_Alumno = nombre_Alumno;
+            Email_Alumno = email_Alumno;
+            Calificacion_Alumno = calificacion_Alumno;
+            if (calificacion_Alumno >= 5) Aprobado = true;
+            else Aprobado = false;
+        }
+        public override string ToString()
+        {
+            if (Aprobado) return $"Estimad@ {Nombre_Alumno}\n \n A tua calificación no curso é: APTO\n \nNoraboa. \n \nO Titor.";
+            else return $"Estimad@ {Nombre_Alumno}\n \nA tua calificación no curso é: NON APTO\n \n Lembra que tes unha recuperación en Setembro.\n \nO Titor.";
+        }
+    }
+}
